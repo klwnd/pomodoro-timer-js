eb3d2fec445f3d16480877127948b0443f653710
Krzysztof Lewandowski <lewandowski.krzysiu@gmail.com>
2022-11-28 22:03:30 +0100

    Add func: timer,reset,play/pause,change state, session

--- pomodoro-timer.js
+++ pomodoro-timer.js
@@ -1,22 +1,124 @@
-let workTime = 1; 
-let breakTime = 5;
+let workTime = 2; 
+let breakTime = 1;
 let minutesLeft = workTime;
 let secondsLeft = 0;
-let state = "work";
-let session;
+let state = "setup";
+let session = 0;
 let isActive = false;
-let timeScreenLeft;
+let pomodoroTimeLeft;
 
 let playButton = document.querySelector("#play");
-playButton.addEventListener("click", playButtonClick);
+playButton.addEventListener("click", pomodoroStartPauseControl);
 
 let resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", reset);
+resetButton.addEventListener("click", pomodoroReset);
 
-let timertStatus = document.querySelector("#status");
-let pomodoroStateLbl = document.querySelector("#title");
+let timerStatus = document.querySelector("#status");
+let sessionCircleDiv = document.querySelectorAll(".session > .circle");
 
-// czy da się zrobić tak, aby zmiana wartości zmiennej wyzwalała event a tym samym event aktualizował by różne miejsca z tą wartością? coś jak c# i INotifyPropertyChanged
+let timeLeftScreen = document.querySelector("#time-left");
 
-let timer;
+let pomodoroTimer;
 
+pomodoroPrepareTime();
+
+function pomodoroStartPauseControl()
+{
+    if(!isActive)
+    {
+        pomodoroTimer = setInterval(pomodoroTimerTick, 1000);
+        isActive = true;
+        timerStatus.innerHTML = "Minutes left";
+        playButton.innerHTML = "pause";
+    }
+    else 
+    {
+        pomodoroTimerTickStop()
+        timerStatus.innerHTML = "Pause";
+    }
+
+   changeState();
+}
+
+function updateTimeOnScreen()
+{
+    timeLeftScreen.innerHTML = pomodoroTimeLeft;
+}
+
+function pomodoroTimerTick() 
+{
+    if(secondsLeft == 0)
+    {
+        minutesLeft--;
+        secondsLeft = 60;
+    }
+
+    secondsLeft--;
+    pomodoroPrepareTime();
+
+    if (minutesLeft == 0 && secondsLeft == 0) changeState();
+}
+
+function pomodoroPrepareTime()
+{
+    if(minutesLeft < 10 && secondsLeft < 10 ) pomodoroTimeLeft = "0" + minutesLeft + ":" + "0" + secondsLeft;
+    else if(minutesLeft < 10) pomodoroTimeLeft = "0" + minutesLeft + ":" + secondsLeft;
+    else if (secondsLeft < 10) pomodoroTimeLeft = minutesLeft + ":" + "0" + secondsLeft;
+    else pomodoroTimeLeft = minutesLeft + ":" + secondsLeft;
+
+    updateTimeOnScreen();
+}
+
+function pomodoroTimerTickStop()
+{
+    clearInterval(pomodoroTimer);
+    isActive = false;
+    playButton.innerHTML = "play_arrow";
+}
+
+function pomodoroReset()
+{
+    pomodoroTimerTickStop();
+    minutesLeft = workTime;
+    secondsLeft = 0;
+    state = "setup";
+    session = 0;
+    pomodoroPrepareTime();
+    timerStatus.innerHTML = "Minutes left";
+    for (let session of sessionCircleDiv)
+    {
+        session.classList.remove("done");
+    }
+}
+
+function changeState()
+{
+    switch (state) 
+    {
+        case "work":
+            pomodoroTimerTickStop();
+            state = "break";
+            minutesLeft = breakTime;
+            break;
+        case "break":
+            pomodoroTimerTickStop();
+            state = "work";
+            minutesLeft = workTime;
+            modifySessionNumber();
+            break;
+        case "setup":
+            state = "work"
+            minutesLeft = workTime;
+            modifySessionNumber();
+            break;
+    }
+
+    secondsLeft = 0;
+    pomodoroPrepareTime();
+}
+
+function modifySessionNumber()
+{
+    session ++;
+    sessionCircleDiv[session-1].classList.add("done");
+}
